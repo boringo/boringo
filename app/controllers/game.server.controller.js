6657855149b6efce571b6ff5396b42d2fae1808b
Spencer Bench <spencer.bench@gmail.com>
2014-12-11 22:42:59 -0700

    Completed /game/<gameID>/join.

--- app/controllers/game.server.controller.js
+++ app/controllers/game.server.controller.js
@@ -8,26 +8,29 @@ var Game = mongoose.model('Game');
 var Board = mongoose.model('Board');
 
 // Generates a game board.
-function generateBoard(TermArray,boardLength,free)
+function generateBoard(game)
 {
+	var terms = game.gameTerms;
+	var length = game.boardLength;
+	var free = game.freeSpace;
 	var indexArray = [];
-	for(var t = 0; t < TermArray.length; t++)
+	for(var t = 0; t < terms.length; t++)
 	{
 		indexArray[t] = t;
 	}
-	var boardArray = new Array(boardLength);
-	var boolBoard = new Array(boardLength);
+	var boardArray = new Array(length);
+	var boolBoard = new Array(length);
 	var index;
 	var i;
 	var j;
-	for(i=0; i<boardLength; i++)
+	for(i=0; i<length; i++)
 	{
-		boardArray[i] = new Array(boardLength);
-		boolBoard[i] = new Array(boardLength);
+		boardArray[i] = new Array(length);
+		boolBoard[i] = new Array(length);
 	}
-	for(i=0; i<boardLength; i++)
+	for(i=0; i<length; i++)
 	{
-		for(j=0; j<boardLength; j++)
+		for(j=0; j<length; j++)
 		{
 			boolBoard[i][j] = false;
 			index = Math.floor(Math.random() * indexArray.length );
@@ -41,16 +44,15 @@ function generateBoard(TermArray,boardLength,free)
 	var freeCoords;
 	if(free === true)
 	{
-		var x = Math.floor(Math.random()*boardLength);
-		var y = Math.floor(Math.random()*boardLength);
+		var x = Math.floor(Math.random()*length);
+		var y = Math.floor(Math.random()*length);
 		boardArray[x][y] = -1;
 		boolBoard[x][y] = true;
 	}
-	var boardInfo = ({
-		board: boardArray,
+	return {
+		tiles: boardArray,
 		tilesSelected: boolBoard
-	});
-	return boardInfo;
+	};
 }
 
 // Creates a game.
@@ -73,11 +75,7 @@ exports.create = function(req, res) {
 			//Create Board, both the term reference board and the selected tiles Board.
 			console.log('Done.');
 			console.log('Creating board...');
-			var boardinfo = generateBoard(body.gameTerms, body.boardLength, body.freeSpace);
-			var board = new Board({
-				tiles: boardinfo.board,
-				tilesSelected: boardinfo.tilesSelected
-			});
+			var board = new Board(generateBoard(game));
 
 			user.CurrentGames.push(game.gameId);
 			game.playerCount ++;
@@ -129,8 +127,72 @@ exports.create = function(req, res) {
 
 // Joins a game.
 exports.join = function(req, res) {
-	// TODO
-	res.json({gameID: req.params.gameID});
+	User.findById(req.session.userId, function(err, user){
+		if (err) {
+	 		return res.status(500).send({
+				message: errorHandler.getErrorMessage(err)
+			});
+		} else {
+			Game.findById(req.params.gameID, function(err, game) {
+				if (err) {
+			 		return res.status(400).send({
+						message: errorHandler.getErrorMessage(err)
+					});
+				} else {
+					console.log({username: user.username, currentGames: user.CurrentGames});
+					if (user.CurrentGames.indexOf(game._id) > -1) {
+						// The user has already joined this game.
+				 		return res.status(400).send({
+							message: 'You have already joined that game.'
+						});
+					} else {
+						// Generate a board for the user.
+						var boardObject = generateBoard(game);
+						var board = new Board(boardObject);
+						var boardIDPair = {
+							userId: user._id,
+							boardId: board._id
+						};
+						game.boardIdPairs.push(boardIDPair);
+						// Update other game fields.
+						game.players.push(user._id);
+						game.playerCount++;
+						// Update the user document.
+						user.CurrentGames.push(game._id);
+						// Save the documents.
+						game.save(function(err) {
+							if (err) {
+						 		return res.status(500).send({
+									message: errorHandler.getErrorMessage(err)
+								});
+							} else {
+								user.save(function(err) {
+									if (err) {
+								 		return res.status(500).send({
+											message: errorHandler.getErrorMessage(err)
+										});
+									} else {
+										// Create and return the response object.
+										var obj = {
+											// Fields from the game document.
+											gameTerms: game.gameTerms,
+											playerCount: game.playerCount,
+											players: game.players,
+											boardLength: game.boardLength,
+											winner: game.winner,
+											// The player's board.
+											board: boardObject
+										};
+										res.json(obj);
+									}
+								});
+							}
+						});
+					}
+				}
+			});
+		}
+	});
 };
 
 // Leaves a game.
