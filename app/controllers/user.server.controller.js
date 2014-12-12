f3356734dabcfa520c448e8f357c79236add7ffd
Spencer Bench <spencer.bench@gmail.com>
2014-12-12 00:50:08 -0700

    Fixed some things in the user controller.

--- app/controllers/user.server.controller.js
+++ app/controllers/user.server.controller.js
@@ -7,7 +7,7 @@ var User = mongoose.model('User');
 
 // Returns information about a user.
 exports.info = function(req, res) {
-		User.findById(req.session.userId, function(err,user){
+	User.findById(req.session.userId, function(err,user){
 	 	if(err){
 	 		return res.status(500).send({
 				message: errorHandler.getErrorMessage(err)
@@ -23,18 +23,14 @@ exports.info = function(req, res) {
 			 	}
 			 	else
 			 	{
-			 		var userData = ({
-			 			userIDOut: neededUser.username,
+			 		var userData = {
+			 			username: neededUser.username,
 			 			wins: neededUser.totalWins,
 			 			losses: neededUser.totalLosses
-			 		});
-			 		
+			 		};
 			 		res.json(userData);
-
 			 	}
 			});
-
 	 	}
 	});
-	
 };
